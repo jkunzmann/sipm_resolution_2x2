9019a287e292bc5207da1245f1b987f9d103c468
jkunzmann <jan.kunzmann@unibe.ch>
2024-05-23 12:01:52 +0200

    Added JSON file read out

--- simp_resolution.py
+++ simp_resolution.py
@@ -1,9 +1,16 @@
+# ///////////////////////////////////////////////////////////////// //
+#                                                                   //
+# Last modifications: 10.05.2024 by Jan Kunzmann, University of Bern//
+#                                                                   //
+# ///////////////////////////////////////////////////////////////// //
+
 import h5py
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy.optimize import curve_fit
 from scipy.optimize import OptimizeWarning
 import os
+import json
 
 def open_hdf5_dataset(file_path, dataset_path):
     """
@@ -182,14 +189,17 @@ def estimate_start_parameters(data):
     p0 = [np.mean(data), np.std(data), 1]
     return p0
 
-def create_parm_file(file_path):
+def create_parm_file(file_path, file_name):
     """
     Creates the new file to save the parameter values.
 
         Args:
     - file_path (str): Path to the file.
     """
-    with open(file_path, 'w') as file:
+    # Create the destination folder if it doesn't exist
+    if not os.path.exists(file_path):
+        os.makedirs(file_path)
+    with open(file_path+"/"+file_name, 'w') as file:
         # Write parameter names
         file.write("file, adc, channel, mu, sigma, A, covariance matrix\n")
 
@@ -228,11 +238,11 @@ def append_parameters_to_file(file_path, data_file_name, adc, channel, params, c
         
         #Write data_file_name, adc and channel to the file
         file.write(data_file_name)
-        file.write(" ,{} ,".format(adc))
-        file.write("{} ,".format(channel))
+        file.write(", {}, ".format(adc))
+        file.write("{}, ".format(channel))
 
         # Write parameters to the file
-        file.write(" {:.6f}, {:.6f}, {:.6f}, ".format(*params))
+        file.write("{:.6f}, {:.6f}, {:.6f}, ".format(*params))
         
         # Write covariance matrix to the file, all on one line with brackets
         file.write("[, ")
@@ -281,14 +291,14 @@ def create_folder(folder_path):
     else:
         print(f"Folder '{folder_path}' already exists.")
 
-def run_fit_over_all_adc_ch(integrated_data, parm_file_name, hdf5_file_path):
+def run_fit_over_all_adc_ch(integrated_data, parm_file_name, hdf5_file_name):
     """
     Runs over all the adc and channels to do the fitting of the gaussian, creates the plots and saves them. The parameters of the fit are saved as well.
 
     Ags:
     - integrated_data (array): data of the integrated waveforms saved in array [Meas, ADC, Channel]
     - parm_file_name (str): name of the file to save the parameters in to
-    - hdf5_file_path (str): name of the input file
+    - hdf5_file_name (str): name of the input file
     """
     #create the folder to save the plots
     plot_folder_name = parm_file_name[:-4]
@@ -299,28 +309,83 @@ def run_fit_over_all_adc_ch(integrated_data, parm_file_name, hdf5_file_path):
         for ch in range(integrated_data.shape[2]):  # third dimension
             print(f'adc: {adc}')
             print(f'ch: {ch}')
-            params, covariance = plot_the_int_distr_for_one_w_fit(integrated_data, hdf5_file_path, plot_folder_name, adc, ch)
+            params, covariance = plot_the_int_distr_for_one_w_fit(integrated_data, hdf5_file_name, plot_folder_name, adc, ch)
             #print("Parameter: ",params)
             #print("Covariance: ",covariance)
-            append_parameters_to_file(parm_file_name, hdf5_file_path, adc, ch, params, covariance)
+            append_parameters_to_file(plot_folder_name+"/"+parm_file_name, hdf5_file_name, adc, ch, params, covariance)
 
-def run_fit_over_one_adc_ch(integrated_data, parm_file_name, hdf5_file_path, adc, ch):
+def run_fit_over_all_adc_ch_from_json(integrated_data, parm_file_name, hdf5_file_name, json_data):
+    """
+    Runs over all the adc and channels to do the fitting of the gaussian, creates the plots and saves them. The parameters of the fit are saved as well.
+
+    Ags:
+    - integrated_data (array): data of the integrated waveforms saved in array [Meas, ADC, Channel]
+    - parm_file_name (str): name of the file to save the parameters in to
+    - hdf5_file_name (str): name of the input file
+    """
+    #create the folder to save the plots
+    plot_folder_name = parm_file_name[:-4]
+    create_folder(plot_folder_name)
+
+    # Iteration over all indices of the second and third dimension, for fitting all of them to a gaussian
+    for adc in range(integrated_data.shape[1]):  # second dimension
+        for ch in range(integrated_data.shape[2]):  # third dimension
+            print(f'adc: {adc}')
+            print(f'ch: {ch}')
+            adc_string_name = rename_adc(adc)
+            #print(adc_string_name)
+            if(select_channel_by_JSON_file(ch, adc_string_name, hdf5_file_name, json_data)):
+                params, covariance = plot_the_int_distr_for_one_w_fit(integrated_data, hdf5_file_name, plot_folder_name, adc, ch)
+                #print("Parameter: ",params)
+                #print("Covariance: ",covariance)
+                append_parameters_to_file(plot_folder_name+"/"+parm_file_name, hdf5_file_path, adc, ch, params, covariance)
+
+def rename_adc(adc):
+    if(adc == 0):
+        adc_string = "0CD94138".lower()
+        return adc_string
+    elif(adc == 1):
+        adc_string = "0CD8D648".lower()
+        return adc_string
+    elif(adc == 2):
+        adc_string = "03222650B3FF".lower()
+        return adc_string
+    elif(adc == 3):
+        adc_string = "0CD913FB".lower()
+        return adc_string   
+    elif(adc == 4):
+        adc_string = "0CD93DB0".lower()
+        return adc_string
+    elif(adc == 5):
+        adc_string = "032226517DFF".lower()
+        return adc_string
+    elif(adc == 6):
+        adc_string = "0cd94149".lower()
+        return adc_string   
+    elif(adc == 7):
+        adc_string = "032225B771FF".lower()
+        return adc_string
+    else:
+        return " "
+
+
+def run_fit_over_one_adc_ch(integrated_data, parm_file_name, hdf5_file_name, adc, ch):
     """
     Runs over one adc and one channel to do the fitting of the gaussian, creates the plots and saves them. The parameters of the fit are saved as well.
 
     Ags:
     - integrated_data (array): data of the integrated waveforms saved in array [Meas, ADC, Channel]
     - parm_file_name (str): name of the file to save the parameters in to
-    - hdf5_file_path (str): name of the input file
+    - hdf5_file_name (str): name of the input file
     - adc (int): adc value int
     - ch (int): channel value int
     """
     plot_folder_name = parm_file_name[:-4]
     create_folder(plot_folder_name)
-    params, covariance = plot_the_int_distr_for_one_w_fit(integrated_data, hdf5_file_path, plot_folder_name, adc, ch)
+    params, covariance = plot_the_int_distr_for_one_w_fit(integrated_data, hdf5_file_name, plot_folder_name, adc, ch)
     #print("Parameter: ",params)
     #print("Covariance: ",covariance)
-    append_parameters_to_file(parm_file_name, hdf5_file_path, adc, ch, params, covariance)
+    append_parameters_to_file(plot_folder_name+"/"+parm_file_name, hdf5_file_name, adc, ch, params, covariance)
 
 def calculate_mean_sigma_from_parm_file(parm_file_name):
     """
@@ -330,7 +395,7 @@ def calculate_mean_sigma_from_parm_file(parm_file_name):
     - param_file_name (str): Name of the parameter file.
 
     Returns:
-    - mean (float or None): Mean of the values in the fourth column if successful, else None.
+    - mean (float or None): Mean of the values in the fifth column if successful, else None.
     - std (float or None): Standard deviation of the values in the fourth column if successful, else None.
     """
     try:
@@ -338,7 +403,10 @@ def calculate_mean_sigma_from_parm_file(parm_file_name):
             # Skip the first row
             next(file)
             # Read values from the fourth column
-            values = [float(line.split(', ')[3]) for line in file]
+            values = []
+            for line in file:
+                if float(line.split(', ')[4]) > 0:
+                    values.append(float(line.split(', ')[4]))
             #print(values)
             # Calculate mean
             mean = sum(values) / len(values)
@@ -387,47 +455,130 @@ def plot_mean_sig_values_with_errors(file_name):
     except (FileNotFoundError, ValueError, IndexError):
         print(f"Error: Unable to read data from {file_name}")
 
-if __name__ == "__main__":    
+def read_json_file(file_path):
+    """
+    Read data from a JSON file.
+        
+    Args:
+    - file_path (str): The path to the JSON file.
+        
+    Returns:
+    - data (dict): The data read from the JSON file.
+    """
+    try:
+        with open(file_path, 'r') as file:
+            data = json.load(file)
+        return data
+    except FileNotFoundError:
+        print(f"Error: File '{file_path}' not found.")
+        return None
+    except json.JSONDecodeError:
+        print(f"Error: File '{file_path}' is not a valid JSON file.")
+        return None
+
+def select_channel_by_JSON_file(channel, adc, hdf5_file_name, json_data):
+    for entry in json_data:
+        if (hdf5_file_name == entry.get("calib_run").rsplit('/')[-1]):
+            json_adc = entry.get("adc_serials")
+            #print(json_adc)
+            local_json_ch = entry.get("local_channels")
+            for i in range(len(json_adc)):
+                #print(type(local_json_ch[i]))
+                #print(type(channel))
+                #print(json_adc[i][:].lower())
+                #print(adc)
+                if(adc == json_adc[i][:].lower()) and (channel == int(local_json_ch[i])): 
+                    print("TRUE")
+                    return True
+            
+    return False
+
+
+if __name__ == "__main__":
+
+    json_file_path = "/home/jan/Universitat_Bern/Doktor/ADC_Viewer/2x2/sipm_resolution/2024.05.08.14.59.25.json"
+    json_data = read_json_file(json_file_path)
+
+    for entry in json_data:
+        hdf5_file_path = entry.get("calib_run").rsplit('/',1)[:-1][0]+"/" #path to the *.hdf5 file
+        hdf5_file_name = entry.get("calib_run").rsplit('/')[-1] #name of the *.hdf5 file
+        #print(hdf5_file_name)
+        
+        parm_file_name=hdf5_file_name[:-5].replace(".", "_")+".csv"
+
+        dataset_path = '/light/wvfm/data/'
+        data = open_hdf5_dataset(hdf5_file_path+"/"+hdf5_file_name, dataset_path)
+        """
+        #plot a specific waveform:
+        meas_nr = 10
+        adc = 2
+        ch = 20
+        plot_the_wvf(data["samples"], meas_nr, adc, ch)
+        """
+        #integrate the data
+        integrated_data = integrate_for_gate(data["samples"],80,120)
+
+        #start saving the data in files
+        create_parm_file(parm_file_name[:-4],parm_file_name)
+
+        run_fit_over_all_adc_ch_from_json(integrated_data, parm_file_name, hdf5_file_name, json_data)
+    
+        m, std = calculate_mean_sigma_from_parm_file(parm_file_name[:-4]+"/"+parm_file_name)
+
+        sig_name = json_file_path.split('/')[-1][:-5]+".csv"
+        sig_name = sig_name.replace(".", "_")
+        create_sigma_mean_file(sig_name)
+        append_parameters_to_sigma_file(sig_name, hdf5_file_name, m, std)
+        plot_mean_sig_values_with_errors(sig_name)
+
+
+
+
+
+    #without JSON
+    """
     hdf5_file_path = 'MiniRun5_1E19_RHC.flow.0001023.FLOW.hdf5'
 
-    parm_file_name="test1.csv"
+    parm_file_name=hdf5_file_path[:-5]+".csv"
 
     #print_hdf5_structure(hdf5_file_path)
     dataset_path = '/light/wvfm/data/'
     data = open_hdf5_dataset(hdf5_file_path, dataset_path)
     #print("Content of the dataset:")
     #print(data["samples"].shape)
-
     """
+    """
+    #plot a specific waveform:
     meas_nr = 10
     adc = 2
     ch = 20
     plot_the_wvf(data["samples"], meas_nr, adc, ch)
     """
-
-    integrated_data = integrate_for_gate(data["samples"],80,120)
-    #print(integrated_data.shape)
-
-    mean_data = mean_for_integral(integrated_data)
-    #print(mean_data.shape)
-    
     """
+    #integrate the data
+    integrated_data = integrate_for_gate(data["samples"],80,120)
+    #take the mean of the data
+    #mean_data = mean_for_integral(integrated_data)
+    """
+    """
+    #plot the integral distribution for one channel
     adc = 2
     ch = 20
     plot_the_int_distr_for_one(integrated_data, adc, ch)
     """
-    
-    #create_parm_file(parm_file_name)
+    #start saving the data in files
+    #create_parm_file(parm_file_name[:-4],parm_file_name)
 
     #run_fit_over_one_adc_ch(integrated_data, parm_file_name, hdf5_file_path, adc, ch)
 
     #run_fit_over_all_adc_ch(integrated_data, parm_file_name, hdf5_file_path)
-
-    m, std = calculate_mean_sigma_from_parm_file(parm_file_name)
+    """
+    m, std = calculate_mean_sigma_from_parm_file(parm_file_name[:-4]+"/"+parm_file_name)
     #print(m)
     #print(std)
-    sig_name = "test_sig1.csv"
+    sig_name = "test_resoulution_sig1.csv"
     create_sigma_mean_file(sig_name)
     append_parameters_to_sigma_file(sig_name, hdf5_file_path, m, std)
     plot_mean_sig_values_with_errors(sig_name)
-    
+
+    """
